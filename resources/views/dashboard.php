07ee72917a7907874a61fc8977a8a9a474d1864a
bimachya <cahyabima990@gmail.com>
2023-11-24 08:09:16 +0700

    first commit

--- resources/views/dashboard.php
+++ resources/views/dashboard.php
@@ -1,6 +1,3 @@
-<p>
-        Nama : Bima Cahya Rizqullah
-    </p>
-    <p>
-        Kelas / Prodi : 1C / Teknologi Informasi
-    </p>
+<p>Nama : Mohammad Walid Habibi</p>
+<p>NIM : 233307083</p>
+<p>Kelas / Prodi : 1C / Teknologi Informasi</p>
